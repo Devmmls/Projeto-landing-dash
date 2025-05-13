356797792d7d4ff62cc0b2b2df2cf84f89044cd4
Devmmls <mauriciojunior2602@gmail.com>
2025-05-12 21:13:10 -0300

    redirecionamento para o dash

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,9 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const botaoDash = document.getElementById("btn-dash");
   const botaoDash1 = document.getElementById("btn-dash-inicial");
   const botaoDash2 = document.getElementById("btn-dash-acesso");
-  if (botaoDash || botaoDash1 || botaoDash2) {
+
+  if (botaoDash) {
     botaoDash.addEventListener("click", function () {
-      window.location.href = "./cadastro/cadastro.html";
+      window.location.href = "./dashboard/dashboard.html";
+    });
+  }
+
+  if (botaoDash1) {
+    botaoDash1.addEventListener("click", function () {
+      window.location.href = "./dashboard/dashboard.html";
+    });
+  }
+
+  if (botaoDash2) {
+    botaoDash2.addEventListener("click", function () {
+      window.location.href = "./dashboard/dashboard.html";
     });
   }
 });
